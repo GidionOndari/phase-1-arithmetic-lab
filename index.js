e48d42989dca463d2b4904c72379ced1759afcee
ondarigidion <gidion.ondari@student.moringaschool.com>
2025-06-13 16:00:17 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// multiply — an equation whose multiplied results will equal 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// random — generates a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// mod — an equation that calculates a remainder that is equal to 4
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+// max — will return 20 as the highest number in the set
+const max = Math.max(1, 3, 4, 20, 10);
